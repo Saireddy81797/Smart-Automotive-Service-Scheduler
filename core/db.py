6ffbdce60287d60bdd877e74f055736644281b75
Byreddy Sai Reddy <saireddybyreddy72@gmail.com>
2025-11-10 08:17:37 +0530

    Update db.py

--- core/db.py
+++ core/db.py
@@ -2,26 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# Use MySQL if provided, else fallback to SQLite
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///smart_scheduler.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    echo=False,
-    future=True
-)
-
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False,
-    future=True
-)
-
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 Base = declarative_base()
 
-
 def init_db():
-    """Create all tables"""
     from . import models  # noqa
     Base.metadata.create_all(bind=engine)
