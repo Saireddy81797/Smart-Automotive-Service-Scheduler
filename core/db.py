36b052071249250769401ccd734336457d704246
Byreddy Sai Reddy <saireddybyreddy72@gmail.com>
2025-11-10 07:40:31 +0530

    Create db.py

---
+++ core/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///smart.db")
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+Base = declarative_base()
+
+
+def init_db():
+from . import models # noqa
+Base.metadata.create_all(bind=engine)
